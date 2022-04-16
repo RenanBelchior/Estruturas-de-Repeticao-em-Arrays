400a744c7ce586775851bc807b5788dc188c7e47
Renan S Belchior <102825017+RenanBelchior@users.noreply.github.com>
2022-04-16 02:40:51 -0300

    Add files via upload

---
+++ OrdemInversa/OrdemInversa.java
@@ -0,0 +1,25 @@
+package OrdemInversa;
+
+import java.util.Scanner;
+
+public class OrdemInversa {
+
+	public static void main(String[] args) {
+		
+		int[] vetor = {-5, -6, 15, 50, 8, 4};
+		
+		System.out.print("Vetor: ");
+		int count = 0;
+		while(count < (vetor.length)) {
+			System.out.println(vetor[count] + " ");
+			count++;
+		}
+		
+		System.out.print("\nVetor: ");
+		for(int i = (vetor.length - 1); i >= 0; i--) {
+			System.out.print(vetor[i] + " ");
+		}
+		
+	}
+
+}
